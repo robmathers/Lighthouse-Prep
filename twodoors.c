8003ec292af3e9c3476a2026de6d5ecc99fe7b12
Rob Mathers <robmathers@gmail.com>
2016-08-15 00:07:38 -0400

    Fixed print -> printf and scan -> scanf

--- twodoors.c
+++ twodoors.c
@@ -11,26 +11,26 @@ int main(int argc, const char * argv[]) {
 
     if (play = 'y') {
 
-        print("\nYou are a prisoner in a room with 2 doors and 2 guards.\n");
-        print("One of the doors will guide you to freedom and behind the other is a hangman --you don't know which is which.\n");
-        print("One of the guards always tells the truth and the other always lies. You don't know which one is the truth-teller or the liar either.\n");
-        print("You have to choose and open one of these doors, but you can only ask a single question to one of the guards.\n");
-        print("What do you ask so you can pick the door to freedom?\n\n");
-        print("\t1.Ask the truth-guard to point to the door of doom.\n");
-        print("\t2.Ask the liar-guard to point to the door of doom.\n");
-        print("\t3.Doesn't matter which one you pick.\n");
-        scan("%f", choice);
+        printf("\nYou are a prisoner in a room with 2 doors and 2 guards.\n");
+        printf("One of the doors will guide you to freedom and behind the other is a hangman --you don't know which is which.\n");
+        printf("One of the guards always tells the truth and the other always lies. You don't know which one is the truth-teller or the liar either.\n");
+        printf("You have to choose and open one of these doors, but you can only ask a single question to one of the guards.\n");
+        printf("What do you ask so you can pick the door to freedom?\n\n");
+        printf("\t1.Ask the truth-guard to point to the door of doom.\n");
+        printf("\t2.Ask the liar-guard to point to the door of doom.\n");
+        printf("\t3.Doesn't matter which one you pick.\n");
+        scanf("%f", choice);
 
         char answer = "No matter which one you choose the guards both tell you which door leads to death, and therefore you can pick the other door.\n";
         switch (choice) {
             case 1:
-                print("c", answer);
+                printf("%c", answer);
                 break;
             case 2:
-                print("c", answer);
+                printf("%c", answer);
                 break;
             case 3:
-                print("c", answer);
+                printf("%c", answer);
                 break;
             default:
                 break;
