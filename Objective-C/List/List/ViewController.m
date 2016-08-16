f57beffae048b475b681356b64032f4091cf58d8
Rob Mathers <robmathers@gmail.com>
2016-08-16 17:19:51 -0400

    Initialize the list of Todo Items

--- Objective-C/List/List/ViewController.m
+++ Objective-C/List/List/ViewController.m
@@ -22,6 +22,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // Initialize todoItems list
+    self.todoItems = [[NSMutableArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning {
